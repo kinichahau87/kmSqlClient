d53d426432504aa520e454d7a81cde210e52896b
kinichahau87 <kevinmorfin@yahoo.com>
2017-07-15 18:13:16 -0500

    added modified changes

--- spec/modelTest.js
+++ spec/modelTest.js
@@ -1,10 +1,24 @@
 var __kmModel = require('../src/kmModel');
-var __extend = require('extend');
 
 describe("A model extension", function() {
-  var extendObj = {};
-  var model = new __kmModel.KModel();  
-  __extend(extendObj, model);
-  extendObj.find();
+  it("should extend", function() {
+    var model = new __kmModel.KModel({
+      'message': '',
+      'client_name': ''
+    });
+
+    model.setViewName('trigtest');
+    model.message = "hello world";
+    model.find(null, function(err, results) {
+
+      it("should find ok", function() {
+        if (err) {
+          fail("error while executing test" + err);
+        }
+        console.log(results);
+        expect(results != null).toBe(true);
+      });
+    });
+  });
 
 });
--- src/kmModel.js
+++ src/kmModel.js
@@ -1,26 +1,54 @@
 var kmClient = require('./index');
+var config = require('config');
 
 const __client = new kmClient.kmmysql({
   connectionLimit: 10
 });
 
-var KModel = function() {
+var KModel = function(args) {
   "use strict";
 
-  function KModel() {
-    this.id = -1;
-    __client.createConnection('localhost', 'root', 'S4t4dmin', 'test', function(err) {
+  this.id = -1;
+  var mThis = __construct();
+  var mViewName = '';
+
+  if (args) {
+    for (var arg in args) {
+      this[arg] = args[arg];
+    }
+  } //end of if
+
+  function __construct() {
+    __client.createConnection(config.get('dbConfig.host'), config.get('dbConfig.user'), config.get('dbConfig.password'), config.get('dbConfig.dbName'), function(err) {
       if (err) {
         throw err;
       }
+      return this;
     });
-    return this;
   } //end of function
 
 
-  this.find = function() {
-    console.log(Object.keys(this));
+  this.find = function(aClause, findCallback) {
+    var sql = "";
+    var properties = [];
+    var values = [];
+    for (var key in this) {
+      if (this.hasOwnProperty(key) && typeof(this[key]) !== "function") {
+        properties.push(key);
+      }
+    } //end of for
+
+    sql = ("SELECT " + properties.join(",") + " FROM " + mViewName).toUpperCase();
+    __client.query(sql, values, function(err, results) {      
+      return findCallback(err, results);
+    }); //end of query
+  }; //end of find
+
+  this.setViewName = function(aViewName) {
+    mViewName = aViewName;
   };
+
+  return this;
 };
 
 module.exports.KModel = KModel;
